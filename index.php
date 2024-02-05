0bfce86bb37ec157a9fec7790c247c25dd4e1b7d
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-02-05 14:55:14 +0100

    Impostato Esercizio

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <header>
+        <h1>
+            PHP Badwords
+        </h1>
+    </header>
+
+    <main>
+        <div class="container">
+            <form action="#" method="GET">
+
+                <label for="textarea">
+                    Scrivi un testo
+                </label>
+
+                <textarea name="textarea" id="textarea" cols="30" rows="10"></textarea>
+
+                <label for="censored">
+                    Scrivi una parola da nascondere
+                </label>
+                <input type="text" name="censored" id="censored" value="">
+            </form>
+        </div>
+    </main>
+</body>
+</html>
